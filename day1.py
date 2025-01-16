26a39e297100795e8918d11734fa87dbcc5ccea2
AbzCodes99 <abdirahman.ali200567@gmail.com>
2025-01-16 16:17:49 +0000

    Initial commit with project files

---
+++ day1.py
@@ -0,0 +1,33 @@
+# task 1
+name = "Abdirahman"
+
+print (name)
+
+# task 2
+name = "Abdirahman Ali"
+age = 19
+city = "London"
+
+print(f"My name is {name}, i'm {age} and live in {city}.")
+
+
+# task 3
+user_name = input("Enter your name: ") 
+
+# task 4
+user_age = input("Enter your age: ")   
+user_city = input("Enter your city: ") 
+
+# Print the formatted output
+print(f"My name is {user_name}, I'm {user_age} years old, and I live in {user_city}.")
+
+
+# stretch task 1
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+num1 = int(num1)
+num2 = int(num2)
+
+print(num1+num2)
+
---
+++ day2.py
@@ -0,0 +1,11 @@
+# task 1 
+def task_1():
+    user_name = input("Enter your name: ") 
+    print(f"Welcome {user_name}")
+
+task_1()
+
+# task 2 "
+def task_2(name, age="Unknown"):
+    return f"{name} is {age} years old"
+
